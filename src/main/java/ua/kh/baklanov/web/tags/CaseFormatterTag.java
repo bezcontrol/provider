94e8d27fabaee38b1eb0d816e49f084292f196ac
bezcontrol <oleksii.baklanov@nure.ua>
2020-05-01 19:35:50 +0300

    custom tag and i18n

--- src/main/java/ua/kh/baklanov/web/tags/CaseFormatterTag.java
+++ src/main/java/ua/kh/baklanov/web/tags/CaseFormatterTag.java
@@ -1,22 +1,26 @@
 package ua.kh.baklanov.web.tags;
 
+import org.apache.log4j.Logger;
+import ua.kh.baklanov.exception.Messages;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
-
 import javax.servlet.jsp.tagext.BodyContent;
 import javax.servlet.jsp.tagext.BodyTagSupport;
-
 import java.io.IOException;
+import java.util.Locale;
 
 
 public class CaseFormatterTag extends BodyTagSupport {
+    private static final Logger LOG = Logger.getLogger(CaseFormatterTag.class);
     private String mCase;
+
+    public CaseFormatterTag(){ }
 
     public void setCase(String pCase) {
         mCase = pCase;
     }
 
-
+    @Override
     public int doAfterBody() throws JspException {
         try {
             BodyContent bc = getBodyContent();
@@ -24,13 +28,14 @@ public class CaseFormatterTag extends BodyTagSupport {
             JspWriter out = bc.getEnclosingWriter();
             if (body != null) {
                 if ("upper".equalsIgnoreCase(mCase)) {
-                    out.print(body.toUpperCase());
+                    out.print(body.toUpperCase(Locale.getDefault()));
                 } else {
-                    out.print(body.toLowerCase());
+                    out.print(body.toLowerCase(Locale.getDefault()));
                 }
             }
         } catch (IOException ioe) {
-            throw new JspException("Error: " + ioe.getMessage());
+            LOG.error(Messages.ERROR_CASE_FORMATTER_TAG, ioe);
+            throw new JspException(Messages.ERROR_CASE_FORMATTER_TAG + ioe.getMessage());
         }
         return SKIP_BODY;
     }
